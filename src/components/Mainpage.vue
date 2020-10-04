8d349bf765ce64f5c86c0d80daaa861e4718c136
cpchia <chawchia@hotmail.co.uk>
2020-10-04 16:40:00 +0800

    Remove boilerplate page and added new main page

---
+++ src/components/Mainpage.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="full">
+    <div class="row align-items-center jumbotron jumbotron-fluid page-header home full">
+      <div class="col-md-12">
+        <div class="container">
+          <h1 class="display-4">Blockchain for Will Writing</h1>
+          <p class="lead">
+            Let Blockchain secures your will
+          </p>          
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Mainpage'
+}
+</script>
+
+
+<style>
+
+</style>
