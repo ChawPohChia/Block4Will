16b0afe3b860d1ab924f3185528d533b9bc8e28a
cpchia <chawchia@hotmail.co.uk>
2020-10-09 17:26:43 +0800

    Creating routing pages
    Create routing and pages (Createwill and Retrievewillpage)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
-<template>  
-  <Mainpage/>
+<template>
+  <div id="nav">
+    <router-link to="/">Mainpage</router-link> |
+    <router-link to="/Createwill">Createwill</router-link> |
+    <router-link to="/Retrievewill">Retrievewill</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import Mainpage from './components/Mainpage.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Mainpage
-  }
-}
-</script>
 
 <style>
 #app {
@@ -20,6 +14,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/components/Retrievewill.vue
+++ src/components/Retrievewill.vue
@@ -6,6 +6,6 @@
 
 <script>
   export default {
-
-  }
+  name: 'Retrievewill'
+}
 </script>
