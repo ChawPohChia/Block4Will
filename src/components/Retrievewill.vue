4ed3594b0b0d6d493a47de40269096cc50d62d2a
cpchia <chawchia@hotmail.co.uk>
2020-10-31 15:44:52 +0800

    Retrieval of ipfs Image using hash
    To retrieve executor image uploaded to ipfs using addressable hash

--- src/components/Retrievewill.vue
+++ src/components/Retrievewill.vue
@@ -1,11 +1,34 @@
 // Retrievewill.vue
 
 <template>
-  <h1>Will Retrieval Page</h1>
+  <div>
+  <h1>Will Retrieval Page</h1>  
+  <img id="executorPhoto"/>    
+  <form v-on:submit.prevent="RetrieveWill()">  
+          <button type="submit" class="btn btn-primary">Submit Your Will</button>
+  </form>
+</div>
 </template>
 
 <script>
   export default {
-  name: 'Retrievewill'
-}
+  name: 'Retrievewill', 
+   data() {
+    return {
+      url: null,
+      executorImageHash:"QmQA6uANxuCAAjQ1wKzB5WAcXqozpYnPPD7XSTTANUyPW8"
+    }
+   },
+   methods: {
+    RetrieveWill() {
+      console.log("Retrieving executor image... ");
+      let ipfsAPI= require('ipfs-http-client');
+      let ipfs = ipfsAPI('localhost','5001',{protocol: 'http'}); 
+      ipfs.cat(this.executorImageHash, (err, data) =>{
+          if (err) console.log(err);       
+          document.getElementById("executorPhoto").src = data.toString();
+      });      
+      }
+   }
+  }
 </script>
